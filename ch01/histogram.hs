a27110e35f76a70b0ec27a0c28211d3823590fe8
raydsameshima <ray.d.sameshima@gmail.com>
2020-09-21 17:22:43 -0400

    fig 1.1 can be obtained via histogram.hs

---
+++ ch01/histogram.hs
@@ -0,0 +1,11 @@
+import qualified Statistics.Sample.Histogram as S
+import qualified Data.Vector as V
+
+main :: IO ()
+main = do
+    let xs = V.fromList [2.1, 2.4, 2.2, 2.3, 2.7, 2.5, 2.4, 2.6, 2.6, 2.9]
+        bins = 5
+        lowerBound = 2.05
+        upperBound = 3.05
+        hist = S.histogram_ bins lowerBound upperBound xs
+    print $ V.toList hist

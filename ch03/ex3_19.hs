71b539068d9c3595c61dbf884499b290acf95113
raydsameshima <ray.d.sameshima@gmail.com>
2020-10-08 10:59:31 -0400

    typo fixed

--- ch03/ex3_19.hs
+++ ch03/ex3_19.hs
@@ -14,5 +14,5 @@ main = do
   putStrLn $ "the patrol will miss during the 0th period: " ++ show (p 0)
   putStrLn $ "the patrol will miss during the 1th period: " ++ show (p 1)
   putStrLn $ "the patrol will miss during the 2th period: " ++ show (p 2)
-  putStrLn $ "the patrol will mill at least once:         " ++ show (1 - p 0)
+  putStrLn $ "the patrol will miss at least once:         " ++ show (1 - p 0)
 
